3530c85d2f7e62b84efedb9eeb27a1927658b244
akshaya <akshaya.pra12@gmail.com>
2025-03-14 11:26:43 +0100

    solved lab

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function UserProfilePage() {
   const userProfile = {
@@ -34,15 +34,17 @@ function UserProfilePage() {
           </>
         )}
 
-        {/* Back button */}    
-        <Link to="/">    
-        <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
-          Back
-        </button>
+        {/* Back button */}
+        <Link to="/">
+          <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            Back
+          </button>
         </Link>
+        
       </div>
     </div>
   );
 }
 
 export default UserProfilePage;
+
